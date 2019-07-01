ae91571f77501a4980310a6a769c23f68c2f83cc
Roman Kananovich <roman.kananovich@gmail.com>
2019-07-01 13:28:46 +0300

    Initial Commit

---
+++ Task4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task4
+//
+//  Created by Roma on 7/1/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task4
+//
+//  Created by Roma on 7/1/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
